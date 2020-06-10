b58447904d62bf9e93e0594eca9e07760b2234e5
Nick Bynum <exander@live.com>
2020-06-09 22:33:40 -0600

    styles working

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,9 +1,20 @@
-window.addEventListener('keydown', function (e) {
-  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = this.document.querySelector(`key[data-key="${e.keyCode}"]`);
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
   // @ts-ignore
-  audio.curreantTime = 0;
+  audio.currentTime = 0;
   // @ts-ignore
   audio.play();
-});
+  key.classList.add('playing');
+};
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+  console.log(this);
+  
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
